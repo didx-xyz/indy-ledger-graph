1da6be59335a8766b819548b874c43b5fb37496b
lohanspies <21362987+lohanspies@users.noreply.github.com>
2021-03-09 12:22:16 +0200

    extracted initial codebase from `indy-ledger-monitor` into this repo

---
+++ graphql/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from starlette.graphql import GraphQLApp
+from schema import schema
+import graphene
+
+app = FastAPI()
+
+app.add_route('/graphql', GraphQLApp(schema=schema))
+
+@app.get('/')
+def ping():
+    return {'API': '/graphql'}
