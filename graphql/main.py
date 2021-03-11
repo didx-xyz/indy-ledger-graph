a3016654492c2763b516ec92962368e7fb480a43
lohanspies <21362987+lohanspies@users.noreply.github.com>
2021-03-11 21:38:14 +0200

    added instrospection endpoints

--- graphql/main.py
+++ graphql/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from starlette.graphql import GraphQLApp
-from schema import schema
+from schema import schema, schema_introspection
 import graphene
+import json
 
 app = FastAPI()
 
@@ -9,4 +12,14 @@ app.add_route('/graphql', GraphQLApp(schema=schema))
 
 @app.get('/')
 def ping():
-    return {'API': '/graphql'}
+    return {'API': '/graphql', 'SCHEMA INTROSPECTION': '/schema_introspection', 'SCHEMA JSON': '/schema_json'}
+
+@app.get('/schema_introspection')
+def schema():
+    return {'schema_str': schema_introspection}
+
+@app.get('/schema_json')
+def schema():
+    # json_data = jsonable_encoder(schema_json)
+    # return JSONResponse(content=json_data)
+    return Response(content=json.dumps(schema_introspection), media_type="application/json")
